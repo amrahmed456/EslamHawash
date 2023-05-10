a6b124fc93aadc079f970b8b05b25fa5a74d2b08
Amr Ahmed <31733896+amrahmed456@users.noreply.github.com>
2023-05-11 00:41:52 +0200

    ..

--- layout/js/main.js
+++ layout/js/main.js
@@ -8,4 +8,54 @@ $(document).ready(function(){
         `);
     });
 
+    // flipping text
+    $(function () {
+        var Flip;
+      
+        Flip = class Flip {
+          constructor(el) {
+            this.el = el;
+            this.el = $(this.el);
+            this.currentStep = 0;
+            $('.next').on('click', $.proxy(this.next, this));
+          }
+      
+          next(event) {
+            var currentStepEl, nextStepEl, nextStepNum;
+            if (event) {
+              event.preventDefault();
+            }
+            nextStepNum = this.currentStep + 1;
+            currentStepEl = this.el.find(`.step${this.currentStep}`);
+            nextStepEl = this.el.find(`.step${nextStepNum}`);
+            if (nextStepEl.length) {
+              currentStepEl.prev().removeClass('down');
+              currentStepEl.removeClass('set');
+              currentStepEl.addClass('down');
+              nextStepEl.addClass('set');
+              nextStepEl.removeClass('down');
+              nextStepEl.next().removeClass('down');
+              return this.currentStep++;
+            } else {
+              // reset to 0
+              this.el.find(".step").removeClass('set');
+              this.el.find(`.step${this.currentStep}`).addClass('down');
+              this.el.find(".step").not(`.step${this.currentStep}`).removeClass('down');
+              this.currentStep = -1;
+              return this.next();
+            }
+          }};
+      
+      
+      
+        $(function () {
+          var f;
+          f = new Flip(document.getElementById('flipper'));
+          return setInterval(function () {
+            return f.next();
+          }, 2000);
+        });
+      
+      });
+
 });
