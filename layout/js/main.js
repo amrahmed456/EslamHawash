7a38303455b13c12df06b19cb2f6964d6ebf81f2
Amr Ahmed <31733896+amrahmed456@users.noreply.github.com>
2023-05-13 01:43:34 +0200

    ..

--- layout/js/main.js
+++ layout/js/main.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
 
+    var rtl = true;
+    if($("html").attr("dir") == 'ltr'){
+      rtl = false;
+    } 
+
     $(".dup-hover-effect").each(function(){
         let text = $(this).text();
         $(this).html(`
@@ -9,15 +14,15 @@ $(document).ready(function(){
     });
 
     // flipping text
-    $(function () {
+
+      if($("#flipper").length > 0){
         var Flip;
-      
+  
         Flip = class Flip {
           constructor(el) {
             this.el = el;
             this.el = $(this.el);
             this.currentStep = 0;
-            $('.next').on('click', $.proxy(this.next, this));
           }
       
           next(event) {
@@ -45,17 +50,109 @@ $(document).ready(function(){
               return this.next();
             }
           }};
-      
-      
-      
-        $(function () {
-          var f;
-          f = new Flip(document.getElementById('flipper'));
-          return setInterval(function () {
-            return f.next();
-          }, 2000);
+          
+          $(function () {
+            var f;
+            f = new Flip(document.getElementById('flipper'));
+            return setInterval(function () {
+              return f.next();
+            }, 2000);
+          });
+      }
+       
+
+
+
+      // open side bar
+      $(".mobile-navigation-opener").on("click" , function(){
+        if($(this).hasClass("is-active")){
+          // close
+          $(this).removeClass("is-active");
+          $("#mobile-sidebar").removeClass("active");
+          $("#mobile-sidebar .main-navigations a").removeClass("active");
+        }else{
+          // open
+          $(this).addClass("is-active");
+          $("#mobile-sidebar").addClass("active");
+          let timer = 70;
+          $("#mobile-sidebar .main-navigations a").each(function(){
+            openASideBar($(this),timer);
+            timer += 70;
+          })
+        }
+      });
+
+      function openASideBar(obj, timer){
+        setTimeout(function(){
+          obj.addClass("active");
+        }, timer);
+      }
+
+      if($(".swiper").length > 0){
+        
+        const swiper = new Swiper('.swiper', {
+          // Optional parameters
+          direction: 'vertical',
+          // If we need pagination
+          pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+          },
+          mousewheel: true,
+          autoHeight:true,
+          speed: 700,
+          effect:'coverflow'
+        
         });
-      
+        swiper.changeLanguageDirection('rtl')
+      }
+     
+      if($(".slider-animated").length > 0){
+        $(".slider-bottom").infiniteslide({
+          speed:20,
+          pauseonhover: false,
+          direction: 'down'
+        });
+        $(".slider-to-right").infiniteslide({
+          speed:25,
+          pauseonhover: false,
+          direction: 'up'
+        });
+      }
+     
+      if($(".portfolio-section").length > 0){
+        var slides = document.getElementsByClassName('carousel-cell');
+        var flkty = new Flickity('.flickity-slider',{
+          prevNextButtons: false,
+          pageDots: true,
+          wrapAround:true,
+          setGallerySize: true,
+          contain:true,
+          rightToLeft:rtl
+        });
+        flkty.on('scroll', function () {
+          flkty.slides.forEach(function (slide, i) {
+            var mul = ( rtl ) ? 1/3 : -1/3;
+            var image = slides[i].querySelector(".overlay");
+            var x = (slide.target + flkty.x) * mul;
+            image.style.backgroundPosition = x + 'px';
+          });
+        });
+
+        $('.flickty-nav-slider').flickity({
+          asNavFor: '.flickity-slider',
+          prevNextButtons: false,
+          pageDots: false,
+          contain:true,
+          rightToLeft:rtl
+        });
+
+      }
+
+      $(".go-to-swipper").on("click" , function(e){
+        e.preventDefault();
+        let child = $(this).attr("data-child");
+        $(".swiper-pagination-bullet:nth-child("+child+")").click();
       });
 
 });
